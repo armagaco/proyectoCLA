e183f2e3a81e45ab8aa6df54d04e094453ad1ec4
armagaco <armagaco@gmail.com>
2016-12-19 07:43:41 -0500

    EnviarMail

--- js/validar.js
+++ js/validar.js
@@ -1,12 +1,16 @@
-function validar() {
-	var nombre, apellidos, correo , clave, usuario, telefono, expresion;
+!function validar() {
+	var nombre, apellidos, correo , clave, usuario, telefono, expresion, mensaje;
 	nombre = document.getElementById("nombre").value;
 	correo = document.getElementById("correo").value;
 	clave = document.getElementById("clave").value;
 	usuario = document.getElementById("usuario").value;
 	telefono = document.getElementById("telefono").value;
-	
-	if (nombre=== ""||correo === "" ||clave=== ""||usuario === "" ||telefono === "") {
+	mensaje = document.getElementById("mensaje").value;
+
+
+	expresion= /\w+@\w+\.+[a-z]/ ;	
+
+	if (nombre== ""||correo == "" ||clave== ""||usuario == "" ||telefono == ""||mensaje == "") {
 		alert("Todos los campos son obligatorios");
 		return false;
 	}
@@ -20,8 +24,19 @@ function validar() {
 		return false;
 		
 	}
+	else if(expresion.test(correo)){
+		alert("EL correo no es valido");
+		return false;
+		
+	}
+
 	else if(clave.length>20){
 		alert("La clave tiene demasiados caracteres");
+		return false;
+		
+	}
+	else if(mensaje.length<5){
+		alert("El mensaje tiene pocos caracteres");
 		return false;
 		
 	}
