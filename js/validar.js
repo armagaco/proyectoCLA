1849d941dfffee5800555e16b030d436e55a8e4e
armagaco <armagaco@gmail.com>
2016-12-19 06:15:23 -0500

    ValidarRegistro

---
+++ js/validar.js
@@ -0,0 +1,28 @@
+function validar() {
+	var nombre, apellidos, correo , clave, usuario, telefono, expresion;
+	nombre = document.getElementById("nombre").value;
+	correo = document.getElementById("correo").value;
+	clave = document.getElementById("clave").value;
+	usuario = document.getElementById("usuario").value;
+	telefono = document.getElementById("telefono").value;
+	
+	if (nombre=== ""||correo === "" ||clave=== ""||usuario === "" ||telefono === "") {
+		alert("Todos los campos son obligatorios");
+		return false;
+	}
+	else if(nombre.length>30){
+		alert("El nombre tiene demasiados caracteres");
+		return false;
+		
+	}
+	else if(correo.length>100){
+		alert("El correo tiene demasiados caracteres");
+		return false;
+		
+	}
+	else if(clave.length>20){
+		alert("La clave tiene demasiados caracteres");
+		return false;
+		
+	}
+};
