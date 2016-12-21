8d022c4777db0a12e2b6d67b9590d08cdee34e6a
armagaco <armagaco@gmail.com>
2016-12-21 02:04:39 -0500

    Update

--- mail.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$destino= $_POST['armagaco@gmail.com'];
-	$asunto= "comentario";
-
-	$comentario = "Email: $_POST [email] Comentario: $_POST [mensaje]";
-	$headers = 'From:'.$_POST['email']."\r\n".'Reply-To:'.$_POST['email']."\r\n".'X-Mailer:PHP/'.phpversion();
-	mail($destino,$asunto, $comentario,$headers);
-	echo "Mensaje enviado";
->?	
