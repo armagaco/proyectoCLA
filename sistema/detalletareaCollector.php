e6c7d9ecac31ae6ad14cfda228fa5081f50ab8ab
eeyc88 <erick-yepez-cedeno@hotmail.com>
2017-01-30 22:44:36 -0500

    archivos.php #7

---
+++ sistema/detalletareaCollector.php
@@ -0,0 +1,21 @@
+<?php
+
+include_once('detalletarea.php');
+include_once('Collector.php');
+
+class DetalletareaCollector extends Collector
+{
+  
+  function showDetalletareas() {
+    $rows = self::$db->getRows("SELECT * FROM detalletarea ");        
+    $arrayDetalletarea= array();        
+    foreach ($rows as $c){
+      $aux = new Detalletarea($c{'idDetalletarea'},$c{'idCabeceratarea'},$c{'idEmpleado'},$c{'idArea'},$c{'cantidad'},$c{'tarifa'});
+      array_push($arrayDetalletarea, $aux);
+    }
+    return $arrayDetalletarea;        
+  }
+
+}
+?>
+
