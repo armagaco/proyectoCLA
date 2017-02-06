6bda3a2de591161c74147f751855923605eb0dea
Campanita-Fucxia <johana-salmeron1@hotmail.com>
2017-02-05 21:02:02 -0500

    panel

--- sistema/menu.php
+++ sistema/menu.php
@@ -1,21 +1,57 @@
 <?php
 session_start();
 ?>
+
+<!DOCTYPE html>
+<html lang ="en">
+	<head>
+		<title>Menú</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+	    
+                <link href="css/estilo.css" rel="stylesheet" >
+                 <link href="css/login.css" rel="stylesheet" >
+
+	</head>
+
+	<body>
+
+
 <?php
+        echo "<div id='panel'>";
+        echo "<div id='welcome'>";
 	echo "Hola Usuario : ";
 	echo '(' . $_SESSION ['Garcia'] . ')';
+        echo "</div>";
 
 	echo "&nbsp;"; 	
 	echo "<a href = 'logout.php' >Salir</a>"; 
+        echo "</div>";
 	echo "<br>";
 	echo "<br>";
+        echo "<div id='menubar'>";
 	echo "MENU DE TABLAS";
+        echo "</div>";
 	echo "<br>";
 
-
-	echo "<br>"; 	
+        echo "<table style='width:50%'>";
+	echo "<tr>";
+        echo "<th>";	
 	echo "<a href = 'readCultivo.php' >tablaCultivo</a>"; 
-	echo "<br>"; 	
+        echo "</th>";
+        echo "<th>";
+        echo "<a href = 'readDemo.php?usuario=$usuario' >Código</a>";
+        echo "</th>";
+        echo "<th>";
+	echo "<a href = 'readDemo.php?usuario=$usuario' >Nombre</a>";
+        echo "</th>";
+        echo "<th>";
+	echo "<a href = 'readDemo.php?usuario=$usuario' >Estado</a>";
+        echo "</th>";
+
+        
+
+		
 	echo "<a href = 'readMedida.php' >tablaMedida</a>"; 
 	echo "<br>"; 	
 	echo "<a href = 'readGrupoLabor.php' >tablaLabores</a>"; 
@@ -23,5 +59,8 @@ session_start();
 	echo "<a href = 'readDepartamento.php' >tablaDepartamento</a>"; 
 	echo "<br>"; 	
 	echo "<a href = 'readCargo.php' >tablaCargo</a>"; 
+        echo "</table>";
 
 ?>
+</body>
+</html>
