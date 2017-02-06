e2f61ee437809685410ae514f9ebb4711e443b70
Campanita-Fucxia <johana-salmeron1@hotmail.com>
2017-02-05 20:06:34 -0500

    labor

--- sistema/laborCollector.php
+++ sistema/laborCollector.php
@@ -16,7 +16,17 @@ class LaborCollector extends Collector
     return $arrayLabor;        
   }
 
+function deleteLabores($idLabor) {
+    $rows = self::$db->deleteRow("DELETE FROM $this->tabla where $this->idLabor = $idLabor", null);             
+  }
+
+function updateLabores($id,$idGrupoLabor,$nombre, $estado) {
+    $rows = self::$db->updateRow("Update $this->tabla set $this->idGrupoLabor = '$idGrupoLabor', $this->nombre = '$nombre', $this->estado = $estado where $this->idLabor =$idLabor", null);             
+  }
+
+ function insertLabores($idGrupoLabor,$nombre, $estado) {
+    $rows = self::$db->insertRow("Insert into $this->tabla ($this->idGrupoLabor, $this->nombre, $this ->estado) values ('$idGrupoLabor' , '$nombre' , '$estado' )" , null);             
+  }
 }
 ?>
-
 
