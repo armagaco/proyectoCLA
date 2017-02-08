e349ec0690a082b98e81208ce02f9bc7fb2a7f53
Campanita-Fucxia <johana-salmeron1@hotmail.com>
2017-02-07 22:52:26 -0500

    modificacion

--- sistema/login.php
+++ sistema/login.php
@@ -1,23 +1,42 @@
 <?php
 session_start();
+?>
 
+ <!DOCTYPE html>
+<html lang ="en">
+	<head>
+		<title>MenuPrincipal</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+	       
+                <link href="../css/login.css" rel="stylesheet" >
+
+	</head>
+
+	<body>
+<?php
+ echo "<div id='panel'>";
 $usuario = $_POST['Nombre']  .$_POST['Apellido'];
 
 	$_SESSION['Garcia'] = $usuario;
 	if (isset($_SESSION['Garcia'])){
-		echo "Hola Usuario :";	
+          echo "<div id='welcome'>";
+		echo "Hola Usuario :";
+          echo "</div>";	
 	}
-	echo '(' . $_SESSION ['Garcia'] . ')';
+	echo '(' . $_SESSION ['Garcia'] . ')';	
+	 	
+	echo "<div id='salir'><a href = 'logout.php' >Salir</a></div>"; 
+ echo"</div>";
+
+echo "<div class='opcion'>";
+echo "<p><a href = 'index.php' >Volver</a></p>"; 
+echo "</div>";
 	
-	echo "&nbsp;";
-	echo "&nbsp;"; 
-	echo "&nbsp;";	
-	echo "<a href = 'index.php' >Volver</a>"; 
-	echo "&nbsp;";
-	echo "&nbsp;"; 
 	//echo "<a href = 'menu.php?usuario=$usuario'> Menú </a>"; 
-	echo "<a href = 'menu.php?usuario'> Menú </a>"; 	
-	echo "&nbsp;";
-	echo "&nbsp;"; 	
-	echo "<a href = 'logout.php' >Salir</a>"; 
+        echo "<div class='opcion'>";
+	echo "<p><a href = 'menu.php?usuario'> Menú </a></p>";
+        echo "</div>"; 
 ?>
+</body>
+</html>
