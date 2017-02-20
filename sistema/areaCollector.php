ba76352ab1c4d03ff33a4d0b394b145165da7c4c
RogerPM <robafiel@hotmail.com>
2017-02-20 02:34:11 -0500

    subir imagenes

--- sistema/areaCollector.php
+++ sistema/areaCollector.php
@@ -7,14 +7,26 @@ class AreaCollector extends Collector
 {
   
   function showAreas() {
-    $rows = self::$db->getRows("SELECT * FROM area ");        
+    $rows = self::$db->getRows("SELECT * FROM controlAgricola.area ");        
     $arrayArea= array();        
     foreach ($rows as $c){
-      $aux = new Area($c{'idArea'},$c{'nombre'},$c{'tamano'},$c{'estado'});
+      $aux = new Area($c{'idarea'},$c{'nombre'},$c{'tamano'},$c{'estado'});
       array_push($arrayArea, $aux);
     }
     return $arrayArea;        
   }
 
+  function deleteAreas($id) {
+    $rows = self::$db->deleteRow("DELETE FROM controlAgricola.area where idarea = $id", null);             
+  }
+
+  function insertAreas($nombre, $tamano, $estado) {
+    $rows = self::$db->insertRow("Insert into controlAgricola.area (nombre, tamano, estado) values ('$nombre' , $tamano , $estado )" , null);             
+  }
+ 
+ function updateAreas($id,$nombre, $tamano, $estado) {
+    $rows = self::$db->updateRow("Update controlAgricola.area set nombre = '$nombre', tamano = $tamano, estado = $estado where idarea =$id", null);             
+  }
+ 
 }
 ?>
