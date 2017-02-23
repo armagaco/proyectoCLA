4c68bf545b3708b7a5bf530e3a92c70509a59a3e
RogerPM <robafiel@hotmail.com>
2017-02-23 06:20:16 -0500

    correccion de comillas

--- sistema/PeriodoCollector.php
+++ sistema/PeriodoCollector.php
@@ -24,12 +24,12 @@ class PeriodoCollector extends Collector
 
   function insertPeriodo($nombre, $fechainicio, $fechafin, $estado) 
   {
-    $rows = self::$db->insertRow("Insert into controlAgricola.periodo (nombre, fechainicio, fechafinal, estado) values ($nombre , $fechainicio , $fechafin , $estado)" , null);             
+    $rows = self::$db->insertRow("Insert into controlAgricola.periodo (nombre, fechainicio, fechafinal, estado) values ('$nombre' , '$fechainicio' , '$fechafin' , $estado)" , null);             
   }
  
  function updatePeriodo($id, $nombre, $fechainicio, $fechafin, $estado) 
  {
-    $rows = self::$db->updateRow("Update controlAgricola.periodo set nombre = $nombre, fechainicio=$fechainicio, fechafinal = $fechafin, estado = $estado where idperiodo =$id", null);             
+    $rows = self::$db->updateRow("Update controlAgricola.periodo set nombre = '$nombre', fechainicio='$fechainicio', fechafinal = '$fechafin', estado = $estado where idperiodo =$id", null);             
  }
  
 }
