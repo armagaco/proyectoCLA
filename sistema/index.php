edb92aea075437bc52d253942dfddcb47b1c5bde
eeyc88 <erick-yepez-cedeno@hotmail.com>
2017-03-05 22:51:18 -0500

    actualizacion del index y js

--- sistema/index.php
+++ sistema/index.php
@@ -10,7 +10,32 @@ session_start();
 		<meta http-equiv= "Content-Type" content= "text/html; charset=UTF-8" />
 		<link rel= "stylesheet" type="text/css" href="../css/bootstrap.css">
 		<link rel= "stylesheet" type="text/css" href="../css/login.css">
-		<script type="text/javascript" src="../js/admin.js"></script>
+		<script type="text/javascript">
+			 function Validar(user,pwd)
+			  {
+			   user=document.getElementById(user);
+			   pwd=document.getElementById(pwd);
+			   if(user.value=="")
+			    {
+			     alert("El campo Nombre esta vacio");
+			     user.focus();
+			     return false;
+			    }
+			   else
+			    {
+			     if(pwd.value=="")
+			      {
+			       alert("El campo Passwd esta vacio");
+			       pwd.focus();
+			       return false;
+			      }
+			     else
+			      {
+			       return true;
+			      }
+			    }
+			  }
+		</script>
 	</head>
 	<body>
 		<div class="jumbotron boxlogin" >
