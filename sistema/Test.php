f672d2a52b27544fcc2258bc5ec44162bf5992ad
eeyc88 <erick-yepez-cedeno@hotmail.com>
2017-03-06 22:24:37 -0500

    terminado

--- sistema/Test.php
+++ sistema/Test.php
@@ -1,10 +1,18 @@
 <?php
-
+include_once('empleado.php');
+include_once('Collector.php');
 
 class Test extends PHPUnit_Framework_TestCase
 {
 	public function testOnePlusOne() {
-		$this->assertEquals(1+1,2);
+		$rows = self::$db->getRows("SELECT * FROM controlAgricola.empleado ");
+		$arrayEmpleado= array();
+		$this->assertNotNull($arrayEmpleado);
+    		foreach ($rows as $c){
+		      $aux = new Empleado($c{'idempleado'},$c{'nombre'},$c{'apellido'},$c{'departamento'},$c{'cargo'});
+		      array_push($arrayEmpleado, $aux);
+		    }
+		    return $arrayEmpleado;
   	}
 }
 ?>
